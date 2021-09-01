9459e620c90185e99b00e2a304accc30c9916686
Shirish Patil <shirishpatil362@gmail.com>
2021-09-01 07:53:40 +0530

    adding new methond to FormatCheck

--- src/main/java/org/shnux/photooganizer/FormatCheck.java
+++ src/main/java/org/shnux/photooganizer/FormatCheck.java
@@ -1,21 +1,23 @@
 package org.shnux.photooganizer;
 
 public class FormatCheck {
-            public static void main(String[] args) {
-              //
-            }
+  public static void main(String[] args) {
+    //
+  }
 
   @Override
   public boolean equals(Object obj) {
-            return                                super.equals(obj);
+    return super.equals(obj);
   }
 
   @Override
   public int hashCode() {
-            return                                super.hashCode();
+    return super.hashCode();
   }
 
   public static void aBetterFormatting() {
     System.out.println("in IDE " + "this is not so cool ");
   }
+
+  public static void addingNewMethod() {}
 }
