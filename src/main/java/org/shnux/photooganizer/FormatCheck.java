7018d33c7a734eeb8df64784a9cdd2514dedf712
wizardwand <shirishpatil362@gmail.com>
2021-09-01 07:52:03 +0530

    un formatted code

--- src/main/java/org/shnux/photooganizer/FormatCheck.java
+++ src/main/java/org/shnux/photooganizer/FormatCheck.java
@@ -1,18 +1,18 @@
 package org.shnux.photooganizer;
 
 public class FormatCheck {
-  public static void main(String[] args) {
-    //
-  }
+            public static void main(String[] args) {
+              //
+            }
 
   @Override
   public boolean equals(Object obj) {
-    return super.equals(obj);
+            return                                super.equals(obj);
   }
 
   @Override
   public int hashCode() {
-    return super.hashCode();
+            return                                super.hashCode();
   }
 
   public static void aBetterFormatting() {
